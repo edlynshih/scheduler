c1c5f3ffd8ca9fc34d22d85de56ba883465be7ed
Edlyn Shih <edlyn0418@gmail.com>
2022-12-01 19:43:59 -0500

    Create DayListItem and import to index.js.

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DayListItem from "components/DayListItem";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -22,3 +23,5 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
+
+  
