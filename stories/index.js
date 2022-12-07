cbf6fdd72161ac7bd2a339f36c19dcc0277c4c98
Edlyn Shih <edlyn0418@gmail.com>
2022-12-07 16:21:33 -0500

    Set up Status story and component

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -147,3 +148,4 @@ storiesOf("Appointment", module)
   .add("Empty", () => <Empty onAdd={action('onAdd')} />)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action('onEdit')} onDelete={action('onDelete')} />)
   .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action('onConfirm')} onCancel={action('onCancel')} />)
+  .add("Status", () => <Status message="Deleting" />)
