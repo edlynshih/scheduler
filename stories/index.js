1fbb1fd0ac87753cea0760a6accb1195bbbcff00
Edlyn Shih <edlyn0418@gmail.com>
2022-12-07 16:07:49 -0500

    Set up show story and component

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -143,3 +144,4 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time='12pm' />)
   .add("Header", () => <Header time='12pm' />)
   .add("Empty", () => <Empty onAdd={action('onAdd')} />)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action('onEdit')} onDelete={action('onDelete')} />)
