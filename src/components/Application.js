692510075498cd71f78a970a88334223e1f572c2
Edlyn Shih <edlyn0418@gmail.com>
2022-12-10 18:36:51 -0500

    Fetching appointmnets api with axios.

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,64 +4,28 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
-    days: []
+    days: [],
+    appointments: {}
   });
+
+  const dailyAppointments = [];
 
   //update day of the state
   const setDay = day => setState({ ...state, day });
 
-  //update setDays of the sate
-  const setDays = (days) => {
-    setState(prev => ({ ...prev, days }));
-  }
+  // setDays([...response.data]);
 
   //using axios to get days api
   useEffect(() => {
-    axios.get(`/api/days`).then(response => {
-      setDays([...response.data]);
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ]).then((all) => {
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data }));
     });
   }, []);
 
@@ -88,7 +52,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map(Appointment)}
+        {dailyAppointments.map(Appointment)}
         <Appointment key={Appointment.id} {...Appointment} />
       </section>
     </main>
