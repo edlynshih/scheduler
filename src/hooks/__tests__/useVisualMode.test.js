659fb60f271bcfc49793fe66377d18377ddb6bcd
Edlyn Shih <edlyn0418@gmail.com>
2022-12-23 21:46:04 -0500

    Build transition and back function for appointments.

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -2,11 +2,60 @@ import { renderHook, act } from "@testing-library/react-hooks";
 
 import useVisualMode from "hooks/useVisualMode";
 
-//empty mode
 const FIRST = "FIRST";
+const SECOND = "SECOND";
+const THIRD = "THIRD";
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
+  expect(result.current.mode).toBe(FIRST);
+});
+
+//test will initialize the mode to FIRST, then transition to SECOND and then check to see what the current value of mode is.
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
+
+//initialize the mode to FIRST then transition to SECOND then transition to THIRD then BACK to SECOND, then BACK to FIRST
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+//the user cannot go back past the initial mode
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+//calling back will skip the SECOND mode
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
   expect(result.current.mode).toBe(FIRST);
 });
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,27 @@
 import { useState } from 'react';
 
+//handle switch between different modes and keep tracks of the previous mode
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+
+    if(replace) {
+      setHistory(prev => [...prev.slice(0, history.length-1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
+  }
+
+  function back() {
+    history.pop();
+
+    if(history.length >= 1) {
+      setMode(history[history.length-1]);
+    }
+  }
+
+  return { mode, transition, back };
 }
